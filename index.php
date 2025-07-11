1ca2ac556d0c4d5818c9c1938daad48a4ea030ed
johann9408 <johan.arteagaa@gmail.com>
2025-07-10 21:24:21 -0500

    change responsive

--- index.php
+++ index.php
@@ -19,9 +19,9 @@
     <link rel="canonical" href="https://mediamaratonentremontanas.com.co">
 
     <!-- CSS -->
-    <link rel="stylesheet" href="styles.css?v=1.0">
-    <link rel="stylesheet" href="./styles/Main.css?v=1.0">
-    <link rel="stylesheet" href="./styles/header.css?v=1.0">
+    <link rel="stylesheet" href="styles.css?v=2025-07-11">
+    <link rel="stylesheet" href="./styles/Main.css?v=2025-07-11">
+    <link rel="stylesheet" href="./styles/header.css?v=2025-07-11">
 
 
     <!-- JavaScript -->
